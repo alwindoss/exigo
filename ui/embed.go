cc65e900d7456e75193744ca95856277bf64ca71
Alwin Doss <alwindoss84@gmail.com>
2022-07-27 22:38:10 +0530

    added ui
    Signed-off-by: Alwin Doss <alwindoss84@gmail.com>

---
+++ ui/embed.go
@@ -0,0 +1,12 @@
+// Package ui handles the PocketBase Admin frontend embedding.
+package ui
+
+import (
+	"embed"
+)
+
+//go:embed dist
+var DistDir embed.FS
+
+//go:embed dist/index.html
+var indexHTML embed.FS
